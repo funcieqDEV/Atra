122ca602a61c865b8b95de98684d679f17ed7522
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2025-06-07 13:41:49 +0200

    added support for void tags!

--- src/codegen/codegen.rs
+++ src/codegen/codegen.rs
@@ -1,19 +1,23 @@
-use crate::parser::node::{ Node};
+use crate::parser::node::Node;
 
 pub fn generate_html(node: &Node, indent: usize, is_root: bool) -> String {
+    let void_tags = vec!["meta", "img", "br", "hr", "input", "link"];
     let mut html = String::new();
-    let indentation = if is_root { "".to_string() } else { "    ".repeat(indent) }; 
+    let indentation = if is_root {
+        "".to_string()
+    } else {
+        "    ".repeat(indent)
+    };
 
     if node.name == "text" {
         if let Some(attr) = node.atributes.iter().find(|a| a.name == "value") {
-            html.push_str(&indentation); 
-            html.push_str(&attr.value.trim_matches('"')); 
+            html.push_str(&indentation);
+            html.push_str(&attr.value.trim_matches('"'));
             html.push('\n');
         }
         return html;
     }
 
-    
     if !node.name.is_empty() {
         html.push_str(&indentation);
         html.push('<');
@@ -24,7 +28,7 @@ pub fn generate_html(node: &Node, indent: usize, is_root: bool) -> String {
             html.push_str(&attr.name);
             html.push('=');
             html.push('"');
-            html.push_str(&attr.value.trim_matches('"')); 
+            html.push_str(&attr.value.trim_matches('"'));
             html.push('"');
         }
 
@@ -33,11 +37,11 @@ pub fn generate_html(node: &Node, indent: usize, is_root: bool) -> String {
     }
 
     for child in &node.children {
-        let child_indent = if is_root { indent } else { indent + 1 }; 
-        html.push_str(&generate_html(child, child_indent, false)); 
+        let child_indent = if is_root { indent } else { indent + 1 };
+        html.push_str(&generate_html(child, child_indent, false));
     }
 
-    if !node.name.is_empty() && node.name != "meta" {
+    if !node.name.is_empty() && !void_tags.contains(&node.name.as_str()) {
         html.push_str(&indentation);
         html.push_str("</");
         html.push_str(&node.name);
